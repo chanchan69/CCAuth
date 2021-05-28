45e74bcf8daea16d38697975cb7b8b86f2fc4af2
chanchan69 <64930470+chanchan69@users.noreply.github.com>
2021-05-28 08:21:59 -0700

    Update auth_module3.py

--- auth_module3.py
+++ auth_module3.py
@@ -5,10 +5,11 @@ import sys
 from time import sleep
 import random
 import string
+from json import loads
 
-aid = ''
-client_secret = ''
-apikey = ''
+aid = 'emWP3kMCf6ALTQis0ogk'
+client_secret = '9dLoBPa0RLIG8ZdvUeGWlHmSpkq1Gg0QClZll7wTHE8='
+apikey = 'IryvtqVncsEm0dS5P7a8uvBycNU0wmry4fZqY6Gu'
 def authenticate(username, password, hwid):
     if int(len(password)) == 0 or int(len(username)) == 0:
         return 'Error: Invalid User or password'
@@ -16,30 +17,36 @@ def authenticate(username, password, hwid):
     f = Fernet(client_secret)
 
     sessionID = (''.join(random.choice(string.ascii_letters + string.digits) for _ in range(40)))
-    payload = f.encrypt(f'{username}:{password}:{hwid}:{sessionID}'.encode())
+    payload = '{"username": "' + username + '", "password": "' + password + '", "hwid": "' + hwid + '", "sessionID": "' + sessionID + '"}'
+    payload = f.encrypt(payload.encode())
 
-    s = requests.get(url=f'https://ccauth.pythonanywhere.com/api/v2/authenticate?key={apikey}', headers={"data": payload, "aid": aid}).text.encode()
+    s = requests.get(url=f'https://api.ccauth.app/api/v3/authenticate?key={apikey}', headers={"data": payload.decode(), "aid": aid}).text
     #This one for no hash ^
-    #s = requests.get(url=f'https://ccauth.pythonanywhere.com/api/v2/authenticate?key={apikey}', headers={"data": payload, "aid": aid, "hash": Get_Hash()}).text.encode()
+    #s = requests.get(url=f'https://api.ccauth.app/api/v3/authenticate?key={apikey}', headers={"data": payload, "aid": aid, "hash": Get_Hash()}).text.encode()
     #This one for hash ^
     
     try:
-        r = str(f.decrypt(s))
+        r = str(f.decrypt(s.encode()).decode())
+        r = loads(r)
     except:
-        if 'Error: Invalid Hash' in str(s):
-            return '4'
+        r = loads(s)
+        if eval(r["error"]):
+            print('an error has occurred because:')
+            print(r["type"])
+            return 'error'
         else:
-            return 'Error: Critical Error'
+            print(r)
+            return 'error'
 
-    if f'{username}:{password} is authenticated {sessionID}' in r:
+    if eval(r["is_Authenticated"]) and eval(r["session_ID"]) == sessionID:
         return '0'
-    elif f'{username}:{password} has wrong hwid' in r:
-        return '1'
-    elif f'{username}:{password} has wrong username or password' in r:
-        return '2'
-    elif 'Error: Bad Everything' in r:
+    elif eval(r["invalid_hwid"]) and eval(r["invalid_credentials"]):
         return '3'
-    elif f'{username}:{password} has an expired license' in r:
+    elif eval(r["invalid_credentials"]) and not eval(r["invalid_hwid"]):
+        return '2'
+    elif not eval(r["invalid_credentials"]) and eval(r["invalid_hwid"]):
+        return '1'
+    elif eval(r["expired_license"]):
         return '4'
     else:
         return 'error'
@@ -48,45 +55,42 @@ def register(username, password, hwid, discordid, registerkey):
     if int(len(password)) == 0 or int(len(username)) == 0:
         return 'Error: Invalid User or password'
     
-    s = requests.get(url=f'https://ccauth.pythonanywhere.com/api/v1/register?key={apikey}', headers={"user": username, "pass": password, "hwid": hwid, "regkey": registerkey, "discord": discordid, "aid": aid}).text.encode()
-    f = Fernet(client_secret)
-    try:
-        r = str(f.decrypt(s))
-    except:
-        return 'Error: Critical Error'
+    r = requests.get(url=f'https://api.ccauth.app/api/v2/register?key={apikey}', headers={"user": username, "pass": password, "hwid": hwid, "regkey": registerkey, "discord": discordid, "aid": aid}).json()
 
-    if f'{username}:{password} registered successfuly' in r:
-        return '0'
-    elif 'bad key' in r:
-        return '1'
+    if not eval(r["error"]):
+        if eval(r["success"]):
+            return '0'
+        elif not eval(r["registration_enabled"]):
+            print('reg not enabled')
+        elif eval(r["invalid_key"]):
+            return '1'
+        elif eval(r["max_users"]):
+            print('max users')
+        else:
+            return 'Error: Critical Error'
     else:
-        return 'Error: Critical Error' 
+        print(r["error"])
 
 def hwid_change(username, password, hwid, hwidresetkey):
     if int(len(password)) == 0 or int(len(username)) == 0:
         return 'Error: Invalid User or password'
-    s = requests.get(url=f'https://ccauth.pythonanywhere.com/api/v2/reset?key={apikey}', headers={"user": username, "pass": password, "newhwid": hwid, "aid": aid, "hwidresetkey": hwidresetkey}).text.encode()
-    f = Fernet(client_secret)
-    try:
-        r = str(f.decrypt(s))
-    except:
-        if 'Error: HWID Resets no Enabled' in str(s):
-            return '3'
-        elif 'Error: Invalid Key' in str(s):
-            return '4'
-        elif 'Error: Already reset today' in str(s):
-            return '5'
-        else:
-            return s
+    r = requests.get(url=f'https://api.ccauth.app/api/v3/reset?key={apikey}', headers={"user": username, "pass": password, "newhwid": hwid, "aid": aid, "hwidresetkey": hwidresetkey}).json()
 
-    if f'{username}:{password} hwid updated successfuly' in r:
-        return '0'
-    elif 'Error: Bad Combo' in r:
-        return '1'
-    elif 'Error: An Error Has Occured' in r:
-        return '2'
+    if not eval(r["error"]):
+        if eval(r["success"]):
+            return '0'
+        elif not eval(r["hwid_resets"]):
+            return '3'
+        elif eval(r["invalid_key"]):
+            return '4'
+        elif eval(r["reset_today"]):
+            return '5'
+        elif eval(r["invalid_credentials"]):
+            return '1'
+        else:
+            return 'error'
     else:
-        return 'error'
+        print(r["error"])
 
 def Get_Hash() -> str:
     hash = ""
